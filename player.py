72031e81227deaba517ba1abc77ebccb79a19cdf
FLOYD-DEV <Christopher.Welton@proton.me>
2025-04-10 13:10:57 -0700

    Added shooting cooldown of 0.3 seconds

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 from shot import Shot
 
 class Player(CircleShape):
@@ -9,6 +9,7 @@ class Player(CircleShape):
         self.rotation = 0
         self.moving_forward = False
         self.moving_backward = False
+        self.shoot_timer = 0  # Initialize shoot timer
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -29,9 +30,11 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        velocity = forward * PLAYER_SHOOT_SPEED
-        shot = Shot(self.position.x, self.position.y, velocity)
+        if self.shoot_timer <= 0:  # Only shoot if timer is 0 or less
+            forward = pygame.Vector2(0, 1).rotate(self.rotation)
+            velocity = forward * PLAYER_SHOOT_SPEED
+            shot = Shot(self.position.x, self.position.y, velocity)
+            self.shoot_timer = PLAYER_SHOOT_COOLDOWN  # Reset timer
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -55,3 +58,7 @@ class Player(CircleShape):
             self.move(dt)
         if self.moving_backward:
             self.move(-dt)
+
+        # Decrease shoot timer
+        if self.shoot_timer > 0:
+            self.shoot_timer -= dt
